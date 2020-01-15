e9b87841f20387dc9e975cf0fb32ba683431e87f
namahu <neti0326@gmail.com>
2020-01-15 20:11:41 +0900

    templateを試し中

--- roibrary.go
+++ roibrary.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
+func createMyRender() multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+	r.AddFromFiles("index", "templates/base.html", "templates/header.html", "templates/index.html", "templates/footer.html")
+	return r
+
+}
+
 func main() {
 	router := gin.Default()
-	router.GET("/", func(ctx *gin.Context) {
-		ctx.String(200, "Hello, World")
+	router.HTMLRender = createMyRender()
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(200, "index", gin.H{
+			"title": "Html5 Template Engine",
+		})
 	})
 
 	router.Run()
