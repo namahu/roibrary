60f6e117160ee18fa6cd6a17b329b524d27b4d1d
namahu <neti0326@gmail.com>
2020-01-15 20:44:41 +0900

    templateの取り込み方を変更

--- roibrary.go
+++ roibrary.go
@@ -1,26 +1,45 @@
 package main
 
 import (
+	"path/filepath"
+
 	"github.com/gin-contrib/multitemplate"
 	"github.com/gin-gonic/gin"
 )
 
-func createMyRender() multitemplate.Renderer {
-	r := multitemplate.NewRenderer()
-	r.AddFromFiles("index", "templates/base.html", "templates/header.html", "templates/index.html", "templates/footer.html")
-	return r
-
-}
-
 func main() {
 	router := gin.Default()
-	router.HTMLRender = createMyRender()
+	router.HTMLRender = loadTemplates("./templates")
 	router.GET("/", func(c *gin.Context) {
-		c.HTML(200, "index", gin.H{
+		c.HTML(200, "index.html", gin.H{
 			"title": "Html5 Template Engine",
 		})
 	})
 
 	router.Run()
+
+}
+
+func loadTemplates(templatsDir string) multitemplate.Renderer {
+	r := multitemplate.NewRenderer()
+
+	layouts, err := filepath.Glob(templatsDir + "/layouts/*.html")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	includes, err := filepath.Glob(templatsDir + "/includes/*.html")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	for _, include := range includes {
+		layoutCopy := make([]string, len(layouts))
+		copy(layoutCopy, layouts)
+		files := append(layoutCopy, include)
+		r.AddFromFiles(filepath.Base(include), files...)
+	}
+
+	return r
 
 }
