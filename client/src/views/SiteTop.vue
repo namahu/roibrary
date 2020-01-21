873f6b5dc1bd7af9d83f51d808ce16c0936bf982
namahu <neti0326@gmail.com>
2020-01-21 20:06:43 +0900

    feat: クライアント側作成中

---
+++ client/src/views/SiteTop.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="siteTop">
+    <div class="sideMenu"></div>
+    <div class="mainContainer"></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Prop, Component } from "vue-property-decorator";
+
+export default class siteTop extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
