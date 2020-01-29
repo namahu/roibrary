05eee43c3302d6f8ce1a27133daf473dcf76840a
namahu <neti0326@gmail.com>
2020-01-29 20:17:02 +0900

    feat: Server側のコードを作成中

--- server/src/roibrary.go
+++ server/src/roibrary.go
@@ -1,20 +1,27 @@
 package main
 
 import (
-	"os"
+	"net/http"
+	"golang.org/x/net/context"
+	firebase "firebase.google.com/go"
+
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"google.golang.org/api/option"
+	"os"
+	"strings"
 )
 
 func getBook(c *gin.Context) {
-	c.String(200, "Hello")
+	c.Writer.Write([]byte("Hello\n"))
 }
 
 func authMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		opt := option.WithCredentialFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS"))
+		opt := option.WithCredentialsFile(os.Getenv("GOOGLE_APPLICATION_CREDENTIALS_DEV"))
 		app, err := firebase.NewApp(context.Background(), nil, opt)
 		if err != nil {
-			fmt.Prinf("error: %v\n", err)
+			fmt.Printf("error: %v\n", err)
 		}
 
 		client, err := app.Auth(context.Background())
@@ -22,18 +29,22 @@ func authMiddleware() gin.HandlerFunc {
 			fmt.Printf("error: %v\n", err)
 		}
 
-		authHeader := router.Header.Get("Authorization")
-		idToken := String.Replace(authHeader, "Bearer ", "", 1)
-		token, err := auth.VerifyIDToken(context.Background(), idToken)
+		authHeader := c.Request.Header.Get("Authorization")
+		idToken := strings.Replace(authHeader, "Bearer ", "", 1)
+		token, err := client.VerifyIDToken(context.Background(), idToken)
 		if err != nil {
 			fmt.Printf("error Verifying ID Token: %v\n", err)
+			c.Writer.WriteHeader(http.StatusUnauthorized)
+			c.Writer.Write([]byte("error verifying ID token\n"))
+			return
 		}
+		fmt.Printf("Verify ID Token: %v\n", token)
 	}
 }
 
 func main() {
 	router := gin.Default()
-	router.Use(authMiddleware)
+	router.Use(authMiddleware())
 
 	router.GET("/getbook", getBook)
 
